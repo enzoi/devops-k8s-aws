e4e037dbf1ffca944ef40a9a343340d9c00839c4
Yeontae Kim <ytk007@gmail.com>
2020-08-20 15:52:32 -0700

    added kubectl cmd to run k8s

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,22 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=ytk007/devops-k8s-aws
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run devops-k8s-aws\
+    --image=$dockerpath\
+    --port=80\
+    --labels app=udacityproject
 
+# Wait 5 seconds until the pod is running
+sleep 5
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward devops-k8s-aws 8080:80
