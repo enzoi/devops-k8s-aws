0acf1a95d0408aee72f1605486c0e6dbfe9bd6b9
Yeontae Kim <ytk007@gmail.com>
2020-08-31 20:15:47 -0700

    add docker hub authentication & tag

--- upload_docker.sh
+++ upload_docker.sh
@@ -2,6 +2,7 @@
 # This file tags and uploads an image to Docker Hub
 
 # Assumes that an image is built via `run_docker.sh`
+# docker build --tag=flask-ml .
 
 # Step 1:
 # Create dockerpath
@@ -10,6 +11,8 @@ dockerpath=ytk007/devops-k8s-aws
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker tag flask-ml $dockerpath
 
 # Step 3:
 # Push image to a docker repository
